9e52834f9e38efcdeb2f2a4381c58e2f0e19ddd5
Alexander Smirnov <furiousteabag@gmail.com>
2023-09-02 00:14:50 -0500

    add prompt

---
+++ data/prompt.py
@@ -0,0 +1,27 @@
+prompt_template = (
+    lambda s: """Write a blog post,Write a 1000-word blog post on content marketing by the end of next week.
+Improve website loading speed,Optimize website loading speed by reducing image sizes and leveraging browser caching within 2 weeks.
+Learn a new programming language,Learn Python programming language through online courses and practice projects within 3 months.
+Increase sales,Achieve a 20% increase in monthly sales by implementing targeted marketing strategies within 6 months.
+Exercise regularly,Engage in at least 30 minutes of aerobic exercise five times a week for the next 3 months.
+Read more books,Read and summarize one industry-related book per month to enhance knowledge and skills.
+Complete online course,Complete the 'Digital Marketing Fundamentals' online course with a score of 90% or higher within 2 weeks.
+Spend less time on social media,Limit daily social media usage to 30 minutes or less to enhance productivity and focus.
+Finish project by deadline,Complete the project plan and deliver all project milestones according to the agreed-upon timeline.
+Get better at public speaking,Improve public speaking skills by participating in a Toastmasters club and delivering at least 5 speeches in the next 3 months.
+Get in shape,Achieve a healthy body weight and fitness level by following a tailored workout and nutrition plan for the next 6 months.
+Travel more,Plan and take a cultural exploration trip to a foreign country within the next year.
+Learn to cook,Master cooking three gourmet dishes by attending a cooking class and practicing regularly over the next 3 months.
+Find a new job,Secure a senior-level software engineering role with a reputable tech company by the end of the year.
+Manage stress,Implement daily mindfulness practices and deep-breathing exercises to effectively manage stress and achieve mental clarity.
+Save money for a vacation,Create a dedicated vacation fund by saving 20% of your monthly income for a dream vacation within the next 9 months.
+Learn a musical instrument,Become proficient in playing the acoustic guitar by practicing at least 1 hour daily and taking advanced lessons for 6 months.
+Become a better listener,Enhance active listening skills through workshops and reflective exercises, contributing to more meaningful conversations.
+Volunteer more,Devote 2 hours each weekend to volunteering at a local shelter for the next 6 months to make a positive impact in the community.
+Learn a new language,Achieve conversational fluency in Spanish by dedicating 30 minutes daily to language practice and immersion activities.
+Write a book,Author a 50,000-word novel within a year by setting aside focused writing time each day and adhering to a structured outline.
+Improve time management,Implement a time-blocking system and prioritize tasks to effectively manage your schedule and achieve work-life balance.
+"""
+    + s
+    + ","
+)
